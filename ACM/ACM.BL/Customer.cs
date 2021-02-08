e9c5dfde7298c1eaf1a9b51839d7ef8f1ec991f6
Edin Jašarević <edinjasar14@gmail.com>
2021-02-08 01:32:58 +0100

    using snippets

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -4,8 +4,21 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; private set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public string EmailAddress { get; set; }
+        
     }
 }
