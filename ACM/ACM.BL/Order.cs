6a6c327b7433a2c755a3222ffc963877d1d9b5b0
Edin Jašarević <edinjasar14@gmail.com>
2021-07-18 23:20:43 +0200

    building the remaining class

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+            
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ ACM/ACM.BL/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal? PurchasePrice { get; set; }
+
+        public int Quatity { get; set; }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quatity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrice == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
