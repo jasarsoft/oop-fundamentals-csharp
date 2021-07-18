b0eea6903ec3822ce6054aa036a93022891a4dca
Edin Jašarević <edinjasar14@gmail.com>
2021-07-18 20:15:21 +0200

    merge

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        public static int InstanceCount { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (String.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (String.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,123 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // -- Arrange
+            var customer = new Customer()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins, Bilbo";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            // -- Arrange
+            var customer = new Customer()
+            {
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            // -- Arrange
+            var customer = new Customer()
+            {
+                LastName = "Bilbo"
+            };
+
+            string expected = "Bilbo";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // -- Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            // -- Act
+
+            // -- Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            // -- arrange
+            var customer = new Customer()
+            {
+                LastName = "Baggins",
+                EmailAddress = "fbaggins@hobbiton.me"
+            };
+
+            var expected = true;
+
+            // -- act
+            var actual = customer.Validate();
+
+            // -- assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastName()
+        {
+            // -- arrange
+            var customer = new Customer()
+            {
+                EmailAddress = "fbaggins@hobbiton.me"
+            };
+
+            var expected = false;
+
+            // -- act
+            var actual = customer.Validate();
+
+            // -- arrange
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
