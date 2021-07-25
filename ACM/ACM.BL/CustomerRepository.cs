14f5ba72209ddc98a2f16f85d078c7af8741fe4b
Edin Jašarević <edinjasar14@gmail.com>
2021-07-25 19:34:03 +0200

    building a repository class

---
+++ ACM/ACM.BL/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+            // temporary hard-coded values
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbition.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+            }
+
+            return customer;
+        }
+
+        public bool Save(Customer customer)
+        {
+            return true;
+        }
+    }
+}
