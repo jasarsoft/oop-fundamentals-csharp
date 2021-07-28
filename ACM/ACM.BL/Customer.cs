d697ca5d227a6573f91869a10d429b4d2e547577
Edin Jašarević <edinjasar14@gmail.com>
2021-07-28 20:08:07 +0200

    composition references

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -8,15 +8,17 @@ namespace ACM.BL
 {
     public class Customer
     {
-        public Customer()
+        public Customer(): this(0)
         {
         }
 
         public Customer(int customerId)
         {
             CustomerId = customerId;
+            AddressList = new List<Address>();
         }
 
+        public List<Address> AddressList { get; set; }
 
         public int CustomerId { get; private set; }
 
