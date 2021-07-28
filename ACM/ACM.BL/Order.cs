394641cbddbf9f75553ff1761f70114f92f7fa1b
Edin Jašarević <edinjasar14@gmail.com>
2021-07-28 20:47:35 +0200

    composition ids

--- ACM/ACM.BL/Order.cs
+++ ACM/ACM.BL/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM.BL
 {
@@ -12,11 +13,18 @@ namespace ACM.BL
         public Order(int orderId)
         {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
         public DateTimeOffset? OrderDate { get; set; }
 
         public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ShippingAddressId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
 
         public bool Validate()
         {
