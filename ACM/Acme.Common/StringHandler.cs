90891588f04732de89466dca48c34e61cb4bea8a
Edin Jašarević <edinjasar14@gmail.com>
2021-10-27 18:39:44 +0200

    building a reusable component

---
+++ ACM/Acme.Common/StringHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Acme.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string soruce)
+        {
+            var result = string.Empty;
+            if (!String.IsNullOrEmpty(soruce))
+            {
+                foreach (var letter in soruce)
+                {
+                    if (char.IsUpper((letter)))
+                    {
+                        result += " ";
+                    }
+
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+    }
+}
